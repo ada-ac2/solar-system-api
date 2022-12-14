fe92aad21f30738f9cf26a83d7874e737f78c262
kelly <kelly.tate.dev@gmail.com>
2022-12-14 14:21:13 -0500

    Added /planets endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description, num_moons):
@@ -7,8 +7,21 @@ class Planet:
         self.description = description
         self.num_moons = num_moons
 
-    planets = [
-        Planet(1, "Mercury", "Mercury is the closest planet to the Sun", 0),
-        Planet(2, "Venus", "Venus is the hottest planet in the solar system", 0 ),
-        Planet(3, "Earth", "Our home planet", 1)
-    ]
+planets = [
+    Planet(1, "Mercury", "Mercury is the closest planet to the Sun", 0),
+    Planet(2, "Venus", "Venus is the hottest planet in the solar system", 0 ),
+    Planet(3, "Earth", "Our home planet", 1)
+]
+
+planets_bp = Blueprint("planets", __name__,url_prefix="/planets")
+@planets_bp.route("", methods=["GET"])
+def get_all_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id" : planet.id,
+            "name" :planet.name,
+            "description" : planet.description,
+            "number of moons" : planet.num_moons
+        })
+    return jsonify(planets_response),200
