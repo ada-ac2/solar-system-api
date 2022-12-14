fb053427dffc71b0a75e1424fa78f45144ca06f7
Eva Liu <una229@gmail.com>
2022-12-14 13:52:37 -0500

    add app folder and planet class in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,13 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, color):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.color = color
+
+list_planets = [
+    Planet(1, "Mercury", "is the smallest planet in the Solar System", "gray"),
+    Planet(3, "Earth", "The planet that we live on.", "blue")
+]
