f7c2f4ab27c1158334553d58cb52922722ad6b98
Xuan Hien Pham <phamxuanhien02@gmail.com>
2022-12-14 14:44:32 -0500

    created planets blueprint

--- app/planets.py
+++ app/planets.py
@@ -1,3 +1,5 @@
+from flask import Blueprint, jsonify
+
 class Planet():
     def __init__(self, id, name, description):
         self.id = id
@@ -9,3 +11,18 @@ planets = [
     Planet(id = 1, name = "Mercury", description = "inhabitable"),
     Planet(id = 4, name = "Mars", description = "inhabitable")
 ]
+
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+
+def get_all_planets():
+    planet_response = []
+    for planet in planets:
+        planet_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+        })
+
+    return jsonify(planet_response)
