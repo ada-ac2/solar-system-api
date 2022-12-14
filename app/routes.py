e5d719c1224c746fe589387f5bf3f062e7b9490d
kelly <kelly.tate.dev@gmail.com>
2022-12-14 14:00:00 -0500

    Created Planets Co-authored-by: Soumya Sah <smysh@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,14 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, num_moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.num_moons = num_moons
+
+    planets = [
+        Planet(1, "Mercury", "Mercury is the closest planet to the Sun", 0),
+        Planet(2, "Venus", "Venus is the hottest planet in the solar system", 0 ),
+        Planet(3, "Earth", "Our home planet", 1)
+    ]
