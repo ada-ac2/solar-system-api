984dd0b27c3ac45ddb2e7fc94d559052ae451f5f
diana <arreoladiana11@gmail.com>
2022-12-14 11:00:29 -0800

    Add planet class

---
+++ app/planet.py
@@ -0,0 +1,7 @@
+
+class Planet:
+    def __init__(self, id, name, description, radius):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.radius = radius
