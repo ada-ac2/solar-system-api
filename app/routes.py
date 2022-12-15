f9c7def89908936b35e9aa9ed15795fc809862f5
annadu <duyuan0921@gmail.com>
2022-12-15 10:58:50 -0800

    Co-authored-by: CatherineBandarchuk <CatherineBandarchuk@users.noreply.github.com> add endpoint by id

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,27 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
-
-from .planets import *
+from .planets import Planet, planets_list
 planets_bp = Blueprint("all_planets", __name__, url_prefix = "/planets")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"massage":f"The planet_id {planet_id} is not valid."}, 400))
+    
+    for planet in planets_list:
+        if planet.id == planet_id:
+            return planet
+    abort(make_response({"massage":f"The planet with id {planet_id} not exist."}, 404))
 
 @planets_bp.route("", methods = ["GET"])
 def get_all_planets():
     planets_response = []
     for planet in planets_list:
-        planets_response.append({
-                "id":planet.id,
-                "name":planet.name,
-                "atmosphere":planet.atmosphere,
-                "diameter":planet.diameter,
-                "description":planet.description
-            })
+        planets_response.append(planet.convert_to_dict())
     return jsonify(planets_response), 200
+
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+def get_planet_by_id(planet_id):
+    planet = validate_planet(planet_id)
+    return jsonify(planet.convert_to_dict(), 200)
