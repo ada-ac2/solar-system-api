15c886577d3f11d1b4cedbc3ab630fea76032431
kelly <kelly.tate.dev@gmail.com>
2022-12-15 13:42:29 -0500

    Made planet module and added attribute

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,4 @@
 from flask import Blueprint, jsonify, abort, make_response
-
-class Planet:
-    def __init__(self, id, name, description, num_moons):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.num_moons = num_moons
-
-    def to_dict(self):
-        return{
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "number of moons": self.num_moons,
-        }
-
 planets = [
     Planet(1, "Mercury", "Mercury is the closest planet to the Sun", 0),
     Planet(2, "Venus", "Venus is the hottest planet in the solar system", 0 ),
