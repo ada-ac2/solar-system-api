f83423666238be775fbacd65bc8ea002e0ff1b39
mckay <mckaybwarren@gmail.com>
2022-12-15 13:26:46 -0500

    create endpoint for getting one planet by id

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,14 @@ class Planet:
         self.description = description
         self.color = color
 
+    def convert_planet_to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "color": self.color
+        }
+
 list_planets = [
     Planet(1, "Mercury", "is the smallest planet in the Solar System", "gray"),
     Planet(3, "Earth", "The planet that we live on.", "blue")
@@ -18,11 +26,13 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 def get_all_planets():
     planets_response = []
     for planet in list_planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "color": planet.color
-        })
+        planets_response.append(planet.convert_planet_to_dict())
 
     return jsonify(planets_response)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet_by_id(planet_id):
+    planet_id = int(planet_id)
+    for planet in list_planets:
+        if planet.id == planet_id:
+            return planet.convert_planet_to_dict()
