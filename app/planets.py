e7051b20669d34c4152f2e533c98eb619d41f648
Lisa Utsett <lisabethutsett@gmail.com>
2022-12-20 15:41:14 -0500

    defined route that reads model records

--- app/planets.py
+++ app/planets.py
@@ -55,6 +55,9 @@ planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
+    if "name" not in request_body or "description" not in request_body or 'diameter_in_km' not in request_body:
+        return make_response("Invalid Request", 400)
+
     new_planet = Planet(name = request_body["name"], 
                         description = request_body["description"], 
                         diameter_in_km = request_body["diameter_in_km"])
@@ -62,3 +65,16 @@ def create_planet():
     db.session.commit()
 
     return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+@planets_bp.route("", methods=["GET"])
+def get_all_planets():
+    planets = Planet.query.all()
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "diameter_in_km": planet.diameter_in_km
+        })
+    return jsonify(planets_response)
