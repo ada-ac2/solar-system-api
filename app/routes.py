842e96dbcebf833d5b441e9e6929fc0f65e44ae6
kelly <kelly.tate.dev@gmail.com>
2022-12-21 13:59:06 -0500

    Added Read, Update, and Delete endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
 from app import db
 from app.models.planet import Planet
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, abort, make_response, request
 
 planets_bp = Blueprint("planets_bp", __name__,url_prefix="/planets")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+    return planet
+
 @planets_bp.route("", methods=["POST"])
 def create_planet_data():
     request_body = request.get_json()
@@ -35,6 +49,42 @@ def read_all_planets():
             }
         )
     return jsonify(planets_response)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def read_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "orbit_days": planet.orbit_days,
+            "num_moons": planet.num_moons
+        }
+
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.orbit_days = request_body["orbit_days"]
+    planet.num_moons = request_body["num_moons"]
+
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully updated")
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully deleted")
+
 
 # ---------------------------Hardcoded Data for Planet------------------------------
 # planets = [
