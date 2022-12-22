87292755b8dd5381cce60d59404c6b5b17565742
kelly <kelly.tate.dev@gmail.com>
2022-12-22 16:38:28 -0500

    Added seed file

---
+++ app/planet_seed.sql
@@ -0,0 +1,9 @@
+ALTER SEQUENCE planet_id_seq RESTART WITH 1;
+INSERT INTO planet VALUES (DEFAULT,'Mercury', 'Mercury is the closest planet to the Sun.', 88, 0);
+INSERT INTO planet VALUES (DEFAULT,'Venus', 'Venus is the hottest planet in the solar system.', 225 ,0);
+INSERT INTO planet VALUES (DEFAULT,'Earth', 'Our home planet.', 365,1);
+INSERT INTO planet VALUES (DEFAULT,'Mars', 'Also known as Red planet.', 687, 2);
+INSERT INTO planet VALUES (DEFAULT,'Jupiter','Largest planet in the solar system.', 4333, 80);
+INSERT INTO planet VALUES (DEFAULT,'Saturn', 'Only planet to have rings made of ice and rock.', 10759, 83);
+INSERT INTO planet VALUES (DEFAULT,'Uranus', 'Only planet with a 97 degree tilted axis.', 30687, 27);
+INSERT INTO planet VALUES (DEFAULT,'Neptune', 'Blue ice giant',60190, 14);
