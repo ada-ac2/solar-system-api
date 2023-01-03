ff76b056128804d188b138e937e34756c2e68fd2
annadu <duyuan0921@gmail.com>
2023-01-03 12:47:44 -0800

    delete class Planet some attributes

--- app/models/planet.py
+++ app/models/planet.py
@@ -4,11 +4,6 @@ from app import db
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String, nullable = False)
-    #livable = db.Column(db.Boolean, default = False, nullable = True) 
-    #number_of_moons = db.Column(db.Integer, nullable = True)
-    length_of_year = db.Column(db.Integer, nullable = False)
-    #namesake = db.Column(db.String, nullable = False)
-    #atmosphere = db.Column(db.String, nullable = False)
-    #diameter = db.Column(db.String, nullable = False)
+    length_of_year = db.Column(db.Integer, nullable = False)   
     description = db.Column(db.String, nullable = False)
     
