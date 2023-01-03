9610dbc7cd81804d8b26568959eb516db48e64fb
diana <arreoladiana11@gmail.com>
2023-01-03 14:27:43 -0800

    Add testing env

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/planet_development'
-
+    
+    if test_config:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+        app.config["Testing"] = True
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    
     db.init_app(app)
     migrate.init_app(app, db)
+
+    from app.models.planet import Planet
 
     from .planet_routes import planets_bp
     app.register_blueprint(planets_bp)
