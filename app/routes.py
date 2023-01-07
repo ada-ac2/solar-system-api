962666cef66cf1e65469fbefcd1c5d1696b7e87a
annadu <duyuan0921@gmail.com>
2023-01-07 15:13:20 -0800

    register planets_bp

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from app import db
+from flask import Blueprint, jsonify, abort, make_response, request 
 from app.model.planet import Planet
-from flask import Blueprint
 
 planets_bp = Blueprint("planets_bp",__name__, url_prefix="/planets")
 
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet.from_dict(request_body)
 
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
