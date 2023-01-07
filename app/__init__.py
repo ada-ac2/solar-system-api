d7ce15535478f86c7976514bb2c35ab94801c401
annadu <duyuan0921@gmail.com>
2023-01-07 15:15:07 -0800

    register planet_bp

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    if test_config:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+        app.config["Testing"] = True
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+    from app.model.planet import Planet
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
 
     return app
