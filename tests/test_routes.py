992d8dca50de876524c691121de868fbd0cbd16f
annadu <duyuan0921@gmail.com>
2023-01-07 16:29:03 -0800

    add get planet by  not exist id return 404

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,10 +2,19 @@ import pytest
 from app.model.planet import Planet
 from app.routes import validate_model
 
-def test_get_planets_empty_db_returns_empty_list(client):
+def test_get_planets_returns_empty_list(client):
     # Act
     response = client.get("/planets")
 
     # Assert 
     assert response.status_code == 200
     assert response.get_json() == []
+
+def test_get_planet_with_id_returns_404(client):
+    # Act
+    response = client.get("/planets/100")
+
+    # Assert 
+    assert response.status_code == 404
+    assert response.get_json() == {"message":f"Planet 100 not found"}
+
