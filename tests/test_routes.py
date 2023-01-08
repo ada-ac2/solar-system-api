6dd1227e16287db66e25ddad4a44c39e111427d6
annadu <duyuan0921@gmail.com>
2023-01-07 16:42:58 -0800

    add test get planets with desc_sort

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,7 +2,7 @@ import pytest
 from app.model.planet import Planet
 from app.routes import validate_model
 
-def test_get_planets_returns_empty_list(client):
+def test_get_planets_no_fixture_returns_empty_list(client):
     # Act
     response = client.get("/planets")
 
@@ -10,7 +10,7 @@ def test_get_planets_returns_empty_list(client):
     assert response.status_code == 200
     assert response.get_json() == []
 
-def test_get_planet_with_id_returns_404(client):
+def test_get_planet_with_id_no_fixture_returns_404(client):
     # Act
     response = client.get("/planets/100")
 
@@ -18,3 +18,34 @@ def test_get_planet_with_id_returns_404(client):
     assert response.status_code == 404
     assert response.get_json() == {"message":f"Planet 100 not found"}
 
+def test_get_planets_returns_seeded_planets(client, two_saved_planets):
+    response = client.get("/planets")
+
+    assert response.status_code == 200
+    planet_list = response.get_json()
+    assert len(planet_list) == 2
+    assert planet_list[0]["id"] == 1
+    assert planet_list[0]["name"] == "Mars"
+    assert planet_list[0]["description"] == "War planet"
+    assert planet_list[0]["radius"] ==  2106.1
+    
+    assert planet_list[1]["id"] == 2
+    assert planet_list[1]["name"] == "Venus"
+    assert planet_list[1]["description"] == "Planet Of Love"
+    assert planet_list[1]["radius"] ==  3760.4
+    
+def test_get_planets_with_desc_sort(client, two_saved_planets):
+    response = client.get("/planets?sort=desc") 
+
+    assert response.status_code == 200
+    planet_list = response.get_json()
+    assert len(planet_list) == 2
+    assert planet_list[0]["id"] == 2
+    assert planet_list[0]["name"] == "Venus"
+    assert planet_list[0]["description"] == "Planet Of Love"
+    assert planet_list[0]["radius"] ==  3760.4
+    
+    assert planet_list[1]["id"] == 1
+    assert planet_list[1]["name"] == "Mars"
+    assert planet_list[1]["description"] == "War planet"
+    assert planet_list[1]["radius"] ==  2106.1
